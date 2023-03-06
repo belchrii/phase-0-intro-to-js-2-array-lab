9f13075acd9828accb63077782bd6638900fd858
chris bell <elmntwzard@gmail.com>
2023-03-06 15:14:36 -0600

    Adds my file.md

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(Ralph) {
+    return cats.push(Ralph);
+}
+function destructivelyPrependCat(Bob) {
+    return cats.unshift(Bob);
+}
+function destructivelyRemoveLastCat(Garfield) {
+    return cats.pop(Garfield);
+}
+function destructivelyRemoveFirstCat(Milo) {
+    return cats.shift(Milo)
+}
+function appendCat(Broom) {
+    return [...cats, Broom];
+}
+function prependCat(Arnold) {
+    return [Arnold, ...cats];
+}
+function removeLastCat() {
+    return cats.slice (0,cats.length-1);
+}
+function removeFirstCat() {
+    return cats.slice (1);
+}
